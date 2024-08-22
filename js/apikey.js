b3d080c2ab53fd8f0529c930308f4e90f8d0e7d2
afteryoon <khsso102649@gmail.com>
2024-08-22 10:46:55 +0900

    init: 프론트 프로젝트

---
+++ js/apikey.js
@@ -0,0 +1,4 @@
+
+const config = {
+  kakao_api:"66862170322262ad6e33fae15b294d56"
+}
---
+++ js/index.js
@@ -0,0 +1,123 @@
+
+window.onload = async () => {
+  try {
+    await loadHTML("header", "layout/header.html");
+    await loadHTML("main", "layout/main.html");
+    await loadHTML("footer", "layout/footer.html");
+
+    let address = '제주특별자치도 제주시 첨단로 330';
+    const coordinates = await getCoordinatesByAddress(address);
+    //console.log(coordinates);
+    initMap(coordinates.x, coordinates.y);
+
+  } catch (error) {
+    console.error("error!!!!!! :"+error);
+  }
+}
+
+const loadHTML = (id, filename) => {
+  fetch(filename)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+const initMap = (x , y) => {
+  const mapContainer  = document.getElementById('map');
+
+  const mapOption  = {
+    center: new kakao.maps.LatLng(y , x),
+    level: 3
+  };
+  const map = new kakao.maps.Map(mapContainer , mapOption);
+
+  const markerPosition  = new kakao.maps.LatLng(y, x);
+  const marker = new kakao.maps.Marker({
+    position: markerPosition
+  });
+
+  marker.setMap(map);
+
+}
+
+
+const getCoordinatesByAddress = (address) => {
+  return new Promise((resolve, reject) => {
+    // 주소-좌표 변환 객체를 생성
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색
+    geocoder.addressSearch(address, (result, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        console.log(result);
+        resolve({
+          x: result[0].x, // 경도
+          y: result[0].y  // 위도
+        });
+      } else {
+        reject(new Error('주소 검색 실패'));
+      }
+    });
+  });
+}
+
+const closeModal = () => {
+  const modal = document.querySelector('#modal');
+  modal.close();
+}
+
+document.addEventListener('click', (event) =>{
+
+  if (event.target.matches('#modal')){
+    const dialogDimensions = event.target.getBoundingClientRect();
+    if (
+      event.clientX < dialogDimensions.left ||
+      event.clientX > dialogDimensions.right ||
+      event.clientY < dialogDimensions.top ||
+      event.clientY > dialogDimensions.bottom
+    ) {
+      event.target.close();
+    }
+  }
+});
+
+
+  document.addEventListener('submit', (event) => {
+
+  if (event.target.matches('form[name="subscription_frm"]')) {
+    event.preventDefault();
+    const email = event.target.email;
+    if(isValidEmail(email.value)){
+      const modal = document.querySelector('#modal');
+      modal.showModal();
+    } else {
+      alert("이메일을 다시 입력하세요!")
+      email.focus();
+    }
+    email.value="";
+  }
+});
+
+const isValidEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+window.addEventListener('scroll', () => {
+
+  const scroll_top_btn = document.querySelector('#scroll_top_btn');
+  // console.log(scrollY);
+  console.log(document.querySelector('#scroll_top_btn').style.visibility);
+  if (window.scrollY > 1) {
+    scroll_top_btn.style.visibility="visible";
+  } else {
+    scroll_top_btn.style.visibility="hidden";
+  }
+});
+
+const toTop = () => {
+  window.scrollTo({
+    top: 0
+    // , behavior: 'smooth'
+  });
+};
